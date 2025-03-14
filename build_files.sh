286ab2e2f6229de9c3b4324a6398ae614054bc09
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2025-03-15 03:46:08 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
 
-if [[$CREATE_SUPERUSER ]];
-then 
-python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
